d6027c639fe313e8e7176c4c3a5a15a9f0524dca
Yannik Scheibner <6142228+YannikS14@users.noreply.github.com>
2021-01-29 14:18:58 +0100

    Add /api Router

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const morgan = require('morgan');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
+const apiRouter = require('./api/api');
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(errorhandler());
 app.use(morgan('dev'));
+
+app.use('/api', apiRouter);
 
 app.listen(PORT, () => {
   console.log('Server is listening on port ' + PORT);
