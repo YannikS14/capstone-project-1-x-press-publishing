acaaee6ff72a3a2f060131d28d686da3dc67e747
Yannik Scheibner <6142228+YannikS14@users.noreply.github.com>
2021-01-29 14:14:52 +0100

    Add bodyParser, cors, errorhandler, morgan and express packages Setup basic express server

---
+++ server.js
@@ -0,0 +1,18 @@
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const errorhandler = require('errorhandler');
+const morgan = require('morgan');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(errorhandler());
+app.use(morgan('dev'));
+
+app.listen(PORT, () => {
+  console.log('Server is listening on port ' + PORT);
+});
+
+module.exports = app;
