c911b2d6d0c850aa3c8c2abaa1333fe17f2f9a33
Yannik Scheibner <6142228+YannikS14@users.noreply.github.com>
2021-01-30 22:35:45 +0100

    Add series delete route

--- api/series.js
+++ api/series.js
@@ -4,6 +4,7 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(
   process.env.TEST_DATABASE || './database.sqlite',
 );
+const issuesRouter = require('./issues');
 
 seriesRouter.get('/', (req, res, next) => {
   db.all('SELECT * FROM Series', (err, series) => {
@@ -94,5 +95,32 @@ seriesRouter.put('/:seriesId', (req, res, next) => {
     },
   );
 });
+
+seriesRouter.delete('/:seriesId', (req, res, next) => {
+  const issueSql =
+    'SELECT * FROM Issue WHERE Issue.series_id = $seriesId';
+  const issueValues = { $seriesId: req.params.seriesId };
+  db.get(issueSql, issueValues, (error, issue) => {
+    if (error) {
+      next(error);
+    } else if (issue) {
+      res.sendStatus(400);
+    } else {
+      const deleteSql =
+        'DELETE FROM Series WHERE Series.id = $seriesId';
+      const deleteValues = { $seriesId: req.params.seriesId };
+
+      db.run(deleteSql, deleteValues, (error) => {
+        if (error) {
+          next(error);
+        } else {
+          res.sendStatus(204);
+        }
+      });
+    }
+  });
+});
+
+seriesRouter.use('/:seriesId/issues', issuesRouter);
 
 module.exports = seriesRouter;
